5197a452ce30d9709955004f94a4fcd20983db54
Karl Heitmann <khf1988@gmail.com>
2022-04-19 20:56:10 -0400

    centra el mapa en santiago

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,8 +8,9 @@ export default class extends Controller {
   connect(){
     console.log("conectando...")
     import("leaflet").then( L => {
+      // OJO: "L" es la librería de "leaflet". 
       console.log("termino de importar leaflet")
-      this.map = L.map(this.placeholderTarget,{zoomDelta:0.5,zoomSnap:0.5}).setView([ 50.1960, -6.8712 ], 10);
+      this.map = L.map(this.placeholderTarget,{zoomDelta:0.5,zoomSnap:0.5}).setView([ -33.41510669581289, -70.63198468627586 ], 10);
       console.log("hasta aqui estamos bien...")
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
