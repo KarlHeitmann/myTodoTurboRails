43d938647c2039b71efb680c4a3a5c0f9c63bb3f
Karl Heitmann <khf1988@gmail.com>
2022-04-19 21:06:15 -0400

    establecida la comunicacion entre el codigo ruby y el codigo javascript

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -9,9 +9,13 @@ export default class extends Controller {
     console.log("conectando...")
     import("leaflet").then( L => {
       // OJO: "L" es la librería de "leaflet". 
-      console.log("termino de importar leaflet")
-      this.map = L.map(this.placeholderTarget,{zoomDelta:0.5,zoomSnap:0.5}).setView([ -33.41510669581289, -70.63198468627586 ], 10);
-      console.log("hasta aqui estamos bien...")
+
+      const lat = this.data.get("lat")
+      const lon = this.data.get("lon")
+      console.log(lat, lon)
+
+      this.map = L.map(this.placeholderTarget,{zoomDelta:0.5,zoomSnap:0.5}).setView([ lat, lon ], 10);
+      
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(this.map);
